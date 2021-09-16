343d1f4b78a20dcbb035219ce7c860eb48298986
NotTerrence <terencedeindugang@gmail.com>
2021-09-16 20:11:24 +0800

    responses updated

--- dbConnect.js
+++ dbConnect.js
@@ -1,3 +1,5 @@
+const { response } = require('express');
+const stories = require('./tempTemplates.json');
 const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('./testDB.db', sqlite3.OPEN_READONLY, (err) => {
@@ -8,37 +10,135 @@ let db = new sqlite3.Database('./testDB.db', sqlite3.OPEN_READONLY, (err) => {
    console.log('| Connected to database |');
    console.log('-------------------------');
 });
+// add SSID (Social Story ID) column in each table for database to accomodate contexts
 
-function WordCount(str) { 
-   return str.split(" ").length;
-}
-
-let template = "$social_story is $adjective and $adjective. There are some things you need to remember when $verb at a $location. If you remember these things, you will have a $adjective time and the $target will be happy that you $verb at the $location.";
-var newTemplate = template.split(" ");
-
-let stmt = `SELECT adjectiveID, message FROM Adjective WHERE adjectiveID  = ?`;
-let re = /[$]adjective/;
-for(i = 0; i < WordCount(template); i++){
-   if(newTemplate[i].includes('$adjective')){
-      let adjectiveID = Math.floor(Math.random() * 13) + 1;
-      let a = newTemplate[i].toString()
-      db.get(stmt, [adjectiveID], (err, row) => {
+function pushTableName() {
+   let tblArr = [];
+   return new Promise((resolve, reject) => {
+      db.all("SELECT name FROM sqlite_master WHERE type = 'table'", [], (err, rows) => {
          if (err) {
             return console.error(err.message);
          }
-         return row
-         ? console.log(a.replace(re, row.message))
-         : console.log(`No playlist found with the id ${adjectiveID}`);
+         rows.forEach((row) => {
+            tblArr.push(row.name.toLowerCase());
+         });
+         resolve(tblArr)
       });
+   })
+}
+
+// async function getTableName() {
+//    try {
+//       row = await pushTableName()
+//       for(i = 0; i < row.length; i++){
+//          tblArr[i] = row[i].toLowerCase();
+//       }
+//    } catch(err) {
+//       console.log(err)
+//    }
+//    console.log(tblArr)
+// }
+
+// let stmt = `SELECT adjectiveID, message FROM Adjective WHERE adjectiveID = ?`;
+// modify main function to dynamically SELECT tables
+// let table = 'Adjective'
+// let rowSql = `SELECT message FROM` + " " + table;
+
+function pushMessage(table) {
+   let rowArr = [];
+   return new Promise((resolve, reject) => {
+      db.all(`SELECT message FROM ` + table, [], (err, rows) => {
+         if (err) {
+            return console.error(err.message);
+         }
+         rows.forEach((row) => {
+            rowArr.push(row.message.toLowerCase());
+         });
+         resolve(rowArr)
+      });
+   })
+}
+// I want to greet the birthday celebrant
+// tell me more
+// what else can you say?
+// async function getMessage() {
+//    try {
+//       row = await pushMessage()
+//       for(i = 0; i < row.length; i++){
+//          rowArr[i] = row[i].toLowerCase();
+//       }
+//    } catch(err) {
+//       console.log(err)
+//    }
+//    console.log(rowArr)
+// }
+
+// async function modTemplate(template) {
+//    // console.log(records)
+//    let tempTemplate = template.split(" ");
+//    try {
+//       records = await pushMessage();
+//       for(i = 0; i < tempTemplate.length; i++){
+//          var j = Math.floor(Math.random() * 13) + 1;
+//          if(tempTemplate[i].includes('$adjective')){
+//             var key = tempTemplate[i].toString()
+//             tempTemplate[i] = key.replace(tag, records[j])
+//             // console.log(tempTemplate[i])
+//          }
+//       }
+//    } catch(err) {
+//       console.log(err)
+//    }
+//    let newTemplate = tempTemplate.join(" ");
+//    console.log(newTemplate)
+// }
+
+let template = stories.social_story.descriptive.shift().split(" ");
+// let template = "<social_story> is $adjective and $adjective".split(" ");
+// let tag = /[$]adjective/;
+// let tableName = []
+// let message = []
+
+async function compareTags() {
+   // console.log(template)
+   try {
+      let tableName = await pushTableName();
+      //let [tableName, message] = await Promise.all([pushTableName(), pushMessage()]); // separate these Promises
+      for(i = 0; i < template.length; i++){
+         if(template[i].includes('$')){
+            for(j = 0; j < tableName.length; j++){
+               if(template[i].includes('$' + tableName[j])){
+                  let table = tableName[j].charAt(0).toUpperCase() + tableName[j].slice(1);
+                  let message = await pushMessage(table);
+                  var id = Math.floor(Math.random() * message.length);
+                  // var key = template[i].toString()
+                  // let tag = /[$]adjective/; // fix tags
+                  // template[i] = key.replace(tag, message[id])
+                  template[i] = message[id]
+                  // console.log(template[i])
+                  // console.log(message)
+                  // console.log(table + ": " + message.length + " id: " + id)
+               }
+            }
+         }
+      }
+      console.log(template.join(" "))
+      // console.log(tableName)
+      // console.log(message)
+      // console.log(template.length)
+      // modTemplate(template)
+   } catch(err) {
+      console.log(err)
    }
 }
 
+compareTags()
 
-db.close((err) => {
-   if (err) {
-      return console.error(err.message);
-   }
-   console.log('------------------------------');
-   console.log('| Closed database connection |');
-   console.log('------------------------------');
-});
+// db.close((err) => {
+//    if (err) {
+//       return console.error(err.message);
+//    }
+//    console.log('------------------------------');
+//    console.log('| Closed database connection |');
+//    console.log('------------------------------');
+// });
